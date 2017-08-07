524768e9953b0f92e06e4338695db02985611f4b
ahmmdd <ahmedmohammedjuned@gmail.com>
2017-08-06 20:16:59 -0400

    Interpolaton & ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  /*
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  */
+  template:`
+  <h1>Welcome to {{title}}!</h1>
+  <p>It certainly does inline!</p>
+  <ul>
+  <li *ngFor="let team of teams; let last = last">{{ team.baseball }} <strong *ngIf="last">Hey, I am last!</strong></li>
+  </ul>`,
+  /*styleUrls: ['./app.component.css']*/
+  styles:[`h1{ color: blue; }`]
 })
 export class AppComponent {
   title = 'app';
+  teams = [
+    {baseball:'Blue Jays'}, 
+    {baseball:'Indians'}, 
+    {baseball:'Yankees'},
+    {baseball:'Mets'}/*,
+    {basketball:'Raptors'}, 
+    {football:'Toronto Maple Leafs'}*/
+  ]
+
 }
